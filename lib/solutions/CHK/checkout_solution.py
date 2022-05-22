99abdb34797ab0df0376bb57d3679697753a1146
sbx_user1051 <sbx_user1051@169.254.236.133>
2022-05-22 16:33:23 +0000

    Sun May 22 16:33:23 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,13 +4,30 @@ skus = "AAAABBCD"
 # skus = unicode string
 def checkout(skus):
     price = {"A": 50, "B": 30, "C": 20, "D": 15}
-
     letters = "ABCD"
+    total = 0
 
     for s in letters:
         count = skus.count(s)
-        print(count)
+        
+        if s == "A":
+            offers = count // 3
+            total += offers * 130
+            non_offers = count % 3
+            total += non_offers * price[s]
+
+        elif s == "B":
+            offers = count // 2
+            total += offers * 45
+            non_offers = count % 2
+            total += non_offers * price[s]
+
+        else:
+            total += count * price[s]
+
+    print(total)
 
 
 checkout(skus)
+
 
