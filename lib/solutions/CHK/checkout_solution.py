3d09b0cf3e1f837aca08d0ba128b49d2e8de6ad4
sbx_user1051 <sbx_user1051@169.254.236.133>
2022-05-22 18:03:07 +0000

    Sun May 22 18:03:07 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -36,40 +36,49 @@ def checkout(skus):
         "Z": 50
     }
 
-    offers = {
-        "AAA": {"price": 130, "saving": 20},
-        "AAAAA": {"price": 200, "saving": 50},
-        "EEB": {"price": 2*items["E"], "saving": items["B"]},
-        "FFF": {"price": 2*items["F"], "saving": items["F"]},
-        "HHHHH": {"price": 45, "saving": 5},
-        "HHHHHHHHHH": {"price": 80, "saving": 20},
-        "KK": {"price": 150, "saving": 10},
-        "NNNM": {"price": 3*items["N"], "saving": items["M"]},
-        "PPPPP": {"price": 200, "saving": 50},
-        "QQQ": {"price": 80, "saving": 10},
-        "RRRQ": {"price": 3*items["R"], "saving": items["Q"]},
-        "UUUU": {"price": 3*items["U"], "saving": items["U"]},
-        "VV": {"price": 90, "saving": 10},
-        "VVV": {"price": 130, "saving": 20},
-    }
+    # offers = {
+    #     "AAA": {"price": 130, "saving": 20},
+    #     "AAAAA": {"price": 200, "saving": 50},
+    #     "EEB": {"price": 2*items["E"], "saving": items["B"]},
+    #     "FFF": {"price": 2*items["F"], "saving": items["F"]},
+    #     "HHHHH": {"price": 45, "saving": 5},
+    #     "HHHHHHHHHH": {"price": 80, "saving": 20},
+    #     "KK": {"price": 150, "saving": 10},
+    #     "NNNM": {"price": 3*items["N"], "saving": items["M"]},
+    #     "PPPPP": {"price": 200, "saving": 50},
+    #     "QQQ": {"price": 80, "saving": 10},
+    #     "RRRQ": {"price": 3*items["R"], "saving": items["Q"]},
+    #     "UUUU": {"price": 3*items["U"], "saving": items["U"]},
+    #     "VV": {"price": 90, "saving": 10},
+    #     "VVV": {"price": 130, "saving": 20},
+    # }
     # offers = [
-    #     {"offer":"AAA", "price": 130, "saving": 20}
-    #     {"offer":"AAAAA": "price": 200, "saving": 50},
-    #     {"offer":"EEB": "price": 2*items["E"], "saving": items["B"]},
-    #     {"offer":"FFF": "price": 2*items["F"], "saving": items["F"]},
-    #     {"offer":"HHHHH": "price": 45, "saving": 5},
-    #     {"offer":"HHHHHHHHHH": "price": 80, "saving": 20},
-    #     {"offer":"KK": "price": 150, "saving": 10},
-    #     {"offer":"NNNM": "price": 3*items["N"], "saving": items["M"]},
-    #     {"offer":"PPPPP": "price": 200, "saving": 50},
-    #     {"offer":"QQQ": "price": 80, "saving": 10},
-    #     {"offer":"RRRQ": "price": 3*items["R"], "saving": items["Q"]},
-    #     {"offer":"UUUU": "price": 3*items["U"], "saving": items["U"]},
-    #     {"offer":"VV": "price": 90, "saving": 10},
-    #     {"offer":"VVV": "price": 130, "saving": 20},
+    #     {"offer":"AAA", "price": 130, "saving": 20},
+    #     {"offer":"AAAAA", "price": 200, "saving": 50},
+    #     {"offer":"EEB", "price": 2*items["E"], "saving": items["B"]},
+    #     {"offer":"FFF", "price": 2*items["F"], "saving": items["F"]},
+    #     {"offer":"HHHHH", "price": 45, "saving": 5},
+    #     {"offer":"HHHHHHHHHH", "price": 80, "saving": 20},
+    #     {"offer":"KK", "price": 150, "saving": 10},
+    #     {"offer":"NNNM", "price": 3*items["N"], "saving": items["M"]},
+    #     {"offer":"PPPPP", "price": 200, "saving": 50},
+    #     {"offer":"QQQ", "price": 80, "saving": 10},
+    #     {"offer":"RRRQ", "price": 3*items["R"], "saving": items["Q"]},
+    #     {"offer":"UUUU", "price": 3*items["U"], "saving": items["U"]},
+    #     {"offer":"VV", "price": 90, "saving": 10},
+    #     {"offer":"VVV", "price": 130, "saving": 20},
     # ]
 
-    offers_sorted = sorted(offers.items(), key=lambda item: item[1]["saving"], reverse=True)
+
+    offers = [
+        {"offer":"AAA", "price": 130, "saving": 20},
+        {"offer":"AAAAA", "price": 200, "saving": 50},
+        {"offer":"EEB", "price": 2*items["E"], "saving": items["B"]},
+        ]
+
+
+
+    offers_sorted = sorted(offers, key=lambda item: item["saving"], reverse=True)
     print(offers_sorted)
 
 
@@ -80,6 +89,7 @@ def checkout(skus):
    
 
 checkout(skus)
+
 
 
 
