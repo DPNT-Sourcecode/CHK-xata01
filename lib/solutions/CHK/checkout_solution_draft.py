ea4fe3932929190c13d4b6f1a71afa33ccd6a9be
sbx_user1051 <sbx_user1051@169.254.236.133>
2022-05-22 17:30:07 +0000

    Sun May 22 17:30:07 UTC 2022

---
+++ lib/solutions/CHK/checkout_solution_draft.py
@@ -0,0 +1,57 @@
+# skus = "EEEBBAAAAAAAAA"
+
+# skus = "EE"
+
+# skus = "FFFFFF"
+
+# noinspection PyUnusedLocal
+# skus = unicode string
+def checkout(skus):
+    price = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+    # item = list(price.keys())
+    item = "AEBCDF"
+
+    total = 0
+    free_Bs = 0
+
+    for s in skus:
+        if s not in item:
+            return(-1)
+
+    for s in item:
+        count = skus.count(s)
+        
+        if s == "A":
+            offer1s = count // 5
+            total += offer1s * 200
+            offer2s = (count % 5) // 3
+            total += offer2s * 130
+            non_offers = (count % 5) % 3
+            total += non_offers * price[s]
+
+        elif s == "E":
+            free_Bs = count // 2
+            total += count * price[s]
+
+        elif s == "B":
+            new_count = count - free_Bs
+            if new_count < 0:
+                pass
+            else:
+                offers = new_count // 2
+                total += offers * 45
+                non_offers = new_count % 2
+                total += non_offers * price[s]
+
+        elif s == "F":
+            offers = count // 3
+            total += offers * 20
+            non_offers = count % 3
+            total += non_offers * price[s]
+
+        else:
+            total += count * price[s]
+
+    return(total)
+
+# checkout(skus)
